76d842774e8aac8df22fc9f4b57fc2dd43420c17
aegrajag <99244022+aegrajag@users.noreply.github.com>
2022-11-29 17:07:41 +0100

    autoformat

--- src/reseau/Serveur.java
+++ src/reseau/Serveur.java
@@ -1,82 +1,83 @@
 package reseau;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-
 public class Serveur {
- 
- public static void main(String[] test) {
-	 
-	 System.out.println("SERVEUR :") ;
- 
-	 final ServerSocket serveurSocket ;
-	 final Socket clientSocket ;
-	 final BufferedReader in;
-	 final PrintWriter out;
-	 final Scanner sc=new Scanner(System.in);
- 
-	 try {
-		 	serveurSocket = new ServerSocket(555);
-		 	System.out.println("Le serveur est à l'écoute");
-		 	
-		 	clientSocket = serveurSocket.accept();
-		 	System.out.println("Connexion acceptée sur le port : " + clientSocket.getPort());
-		 	
-		 	out = new PrintWriter(clientSocket.getOutputStream());
-		 	in = new BufferedReader (new InputStreamReader (clientSocket.getInputStream()));
- 
-		 	Thread envoi= new Thread(new Runnable() {
-		 			String message;
-		 			@Override
-		 			public void run() {
-		 				while(true){
-		 					message = sc.nextLine();
-		 					out.println(message);
-		 					out.flush();
-		 					if (message.equals("bye")) {
-								System.out.println("Fin de la connexion") ;
-								break ;
+
+	public static void main(String[] test) {
+
+		System.out.println("SERVEUR :");
+
+		final ServerSocket serveurSocket;
+		final Socket clientSocket;
+		final BufferedReader in;
+		final PrintWriter out;
+		final Scanner sc = new Scanner(System.in);
+
+		try {
+			serveurSocket = new ServerSocket(555);
+			System.out.println("Le serveur est à l'écoute");
+
+			clientSocket = serveurSocket.accept();
+			System.out.println("Connexion acceptée sur le port : " + clientSocket.getPort());
+
+			out = new PrintWriter(clientSocket.getOutputStream());
+			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+			Thread envoi = new Thread(new Runnable() {
+				String message;
+
+				@Override
+				public void run() {
+					while (true) {
+						message = sc.nextLine();
+						out.println(message);
+						out.flush();
+						if (message.equals("bye")) {
+							System.out.println("Fin de la connexion");
+							break;
+						}
+					}
+				}
+			});
+			envoi.start();
+
+			Thread recevoir = new Thread(new Runnable() {
+				String message;
+
+				@Override
+				public void run() {
+					try {
+						message = in.readLine();
+						// Avec le client connecté :
+						while (message != null) {
+							System.out.println("Client : " + message);
+							if (message.equals("bye")) {
+								System.out.println("Fin de la connexion");
+								break;
 							}
-		 				}
-		 			}
-		 	});
-		 	envoi.start();
- 
-		 	Thread recevoir= new Thread(new Runnable() {
-		 		String message ;
-		 		@Override
-		 		public void run() {
-		 			try {
-		 				message = in.readLine();
-		 				//Avec le client connecté :
-		 				while(message!=null){
-		 					System.out.println("Client : "+message);
-		 					if (message.equals("bye")) {
-		 						System.out.println("Fin de la connexion") ;
-		 						break ;
-							}
-		 					message = in.readLine();
-		 					
-		 				}
-		 				//On ferme la connexion
-		 				out.close();
-		 				clientSocket.close();
-		 				serveurSocket.close();
-		 				System.exit(-1);
-		 			} catch (IOException e) {
-		 				e.printStackTrace();
-		 			}
-		 		}
-		 	});
-		 	recevoir.start();
-	 }catch (IOException e) {
-		 e.printStackTrace();
-	 }
- 	}
+							message = in.readLine();
+
+						}
+						// On ferme la connexion
+						out.close();
+						clientSocket.close();
+						serveurSocket.close();
+						System.exit(-1);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+			recevoir.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
+}
--- src/reseau/client.java
+++ src/reseau/client.java
@@ -1,75 +1,76 @@
 package reseau;
 
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
-import java.io.IOException;
 
 public class client {
 
- public static void main(String[] args) {
-	 
-	 System.out.println("CLIENT :") ;
- 
-	 final Socket clientSocket;
-	 final BufferedReader in;
-	 final PrintWriter out;
-	 final Scanner sc = new Scanner(System.in);
- 
-	 try {
+	public static void main(String[] args) {
 
-		 clientSocket = new Socket("localhost",555);
- 
-		 out = new PrintWriter(clientSocket.getOutputStream());
-		 
-		 in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
- 
-		 Thread envoyer = new Thread(new Runnable() {
-			 String message;
-			 @Override
-			 public void run() {
-				 while(true){
-					 message = sc.nextLine();
-					 out.println(message);
-					 out.flush();
-					 if (message.equals("bye")) {
-							System.out.println("Fin de la connexion") ;
-							break ;
+		System.out.println("CLIENT :");
+
+		final Socket clientSocket;
+		final BufferedReader in;
+		final PrintWriter out;
+		final Scanner sc = new Scanner(System.in);
+
+		try {
+			clientSocket = new Socket("localhost", 555);
+
+			out = new PrintWriter(clientSocket.getOutputStream());
+
+			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+			Thread envoyer = new Thread(new Runnable() {
+				String message;
+
+				@Override
+				public void run() {
+					while (true) {
+						message = sc.nextLine();
+						out.println(message);
+						out.flush();
+						if (message.equals("bye")) {
+							System.out.println("Fin de la connexion");
+							break;
 						}
-				 }
-			 }
-		 });
-		 envoyer.start();
- 
-		 Thread recevoir = new Thread(new Runnable() {
-			 String message;
-			@Override
-			 public void run() {
-				 try {
-					 message = in.readLine();
-					 while(message!=null){
-		 					System.out.println("Serveur : "+message);
-		 					if (message.equals("bye")) {
-								System.out.println("Fin de la connexion") ;
-								break ;
+					}
+				}
+			});
+			envoyer.start();
+
+			Thread recevoir = new Thread(new Runnable() {
+				String message;
+
+				@Override
+				public void run() {
+					try {
+						message = in.readLine();
+						while (message != null) {
+							System.out.println("Serveur : " + message);
+							if (message.equals("bye")) {
+								System.out.println("Fin de la connexion");
+								break;
 							}
-		 					message = in.readLine();
-		 					
-		 				}
-					 out.close();
-					 clientSocket.close();
-					 System.exit(-1);
-				 } catch (IOException e) {
-					 e.printStackTrace();
-				 }
-			 }
-		 });
-		 recevoir.start();
- 
-	 } catch (IOException e) {
-		 e.printStackTrace();
-	 }
- 	}
+							message = in.readLine();
+
+						}
+						out.close();
+						clientSocket.close();
+						System.exit(-1);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+			recevoir.start();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
