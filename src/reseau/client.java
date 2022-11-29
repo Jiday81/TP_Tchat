7724231eb11d0e9aebbda748ba43fadc4bf75a6b
JDAX81 <jd.crooss81@gmail.com>
2022-11-29 17:05:25 +0100

    Reconstruction pour envoie message + bye

--- src/reseau/client.java
+++ src/reseau/client.java
@@ -1,41 +1,75 @@
 package reseau;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.util.Scanner;
+import java.io.IOException;
 
 public class client {
 
-	public static void main(String[] args) throws IOException {
+ public static void main(String[] args) {
+	 
+	 System.out.println("CLIENT :") ;
+ 
+	 final Socket clientSocket;
+	 final BufferedReader in;
+	 final PrintWriter out;
+	 final Scanner sc = new Scanner(System.in);
+ 
+	 try {
 
-		System.out.println("CLIENT :");
-
-		try {
-			Socket echoSocket = new Socket("localhost", 555);
-			DataOutputStream dOut = new DataOutputStream(echoSocket.getOutputStream());
-			DataInputStream dIn = new DataInputStream(echoSocket.getInputStream());
-			dOut.writeUTF("test");
-			dOut.flush();
-			dOut.writeUTF("test2");
-			dOut.flush();
-			dOut.writeUTF("test3");
-			dOut.flush();
-			dOut.writeUTF("bye");
-			dOut.flush();
-			dOut.writeUTF("test");
-			dOut.flush();
-
-			dOut.close();
-			dIn.close();
-			echoSocket.close();
-		} catch (UnknownHostException e) {
-			System.out.println(" Destination localhost inconnue ");
-			System.exit(-1);
-		} catch (IOException e) {
-			System.out.println(" now to investigate this IO issue ");
-			System.exit(-1);
-		}
-	}
+		 clientSocket = new Socket("localhost",555);
+ 
+		 out = new PrintWriter(clientSocket.getOutputStream());
+		 
+		 in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+ 
+		 Thread envoyer = new Thread(new Runnable() {
+			 String message;
+			 @Override
+			 public void run() {
+				 while(true){
+					 message = sc.nextLine();
+					 out.println(message);
+					 out.flush();
+					 if (message.equals("bye")) {
+							System.out.println("Fin de la connexion") ;
+							break ;
+						}
+				 }
+			 }
+		 });
+		 envoyer.start();
+ 
+		 Thread recevoir = new Thread(new Runnable() {
+			 String message;
+			@Override
+			 public void run() {
+				 try {
+					 message = in.readLine();
+					 while(message!=null){
+		 					System.out.println("Serveur : "+message);
+		 					if (message.equals("bye")) {
+								System.out.println("Fin de la connexion") ;
+								break ;
+							}
+		 					message = in.readLine();
+		 					
+		 				}
+					 out.close();
+					 clientSocket.close();
+					 System.exit(-1);
+				 } catch (IOException e) {
+					 e.printStackTrace();
+				 }
+			 }
+		 });
+		 recevoir.start();
+ 
+	 } catch (IOException e) {
+		 e.printStackTrace();
+	 }
+ 	}
 }
