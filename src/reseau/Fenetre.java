79414daaa9608f0414bc32232a911979d51094dd
JDAX81 <jd.crooss81@gmail.com>
2022-12-08 19:34:57 +0100

    update fenêtre

--- src/reseau/Fenetre.java
+++ src/reseau/Fenetre.java
@@ -15,6 +15,7 @@ import javax.swing.JTextField;
 public abstract class Fenetre extends JFrame {
 
 	private static final long serialVersionUID = -8249567620182176669L;
+	private JLabel l1, l2,l3;
 	private JTextArea jta1;
 	private JTextArea jta2;
 	protected JTextField jtf;
@@ -28,16 +29,15 @@ public abstract class Fenetre extends JFrame {
 		this.setLayout(new BorderLayout());
 
 		JPanel p1 = new JPanel(new GridLayout(1, 2));
-		JPanel p2 = new JPanel(new GridLayout(1, 2));
-		JPanel p3 = new JPanel(new GridLayout(2, 1));
-
-		JLabel l1 = new JLabel("TEXTE EN CLAIR :");
-		JLabel l2 = new JLabel("TEXTE CRYPTE :");
-		JLabel l3 = new JLabel("MESSAGE A ENVOYER :");
+		JPanel p2 = new JPanel(new BorderLayout());
+		JPanel p3 = new JPanel(new GridLayout(1, 2));
 
 		this.jta1 = new JTextArea();
 		this.jta2 = new JTextArea();
 		this.jtf = new JTextField();
+		this.l1 = new JLabel("Message reçu : ");
+		this.l2 = new JLabel("Message reçu crypté : ");
+		this.l3 = new JLabel("Ecrivez votre message ici : ");
 
 		this.jtf.setToolTipText("Entrez votre message.");
 
@@ -51,16 +51,16 @@ public abstract class Fenetre extends JFrame {
 		JScrollPane s2 = new JScrollPane(jta2, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
 				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
-		p1.add(l1);
-		p1.add(l2);
-		p2.add(s1);
-		p2.add(s2);
-		p3.add(l3);
-		p3.add(jtf);
+		p1.add(s1);
+		p1.add(s2);
+		p2.add(l3,BorderLayout.NORTH);
+		p2.add(jtf,BorderLayout.CENTER);
+		p3.add(l1);
+		p3.add(l2);
 
-		this.add(p1, BorderLayout.NORTH);
-		this.add(p2, BorderLayout.CENTER);
-		this.add(p3, BorderLayout.SOUTH);
+		this.add(p1, BorderLayout.CENTER);
+		this.add(p2, BorderLayout.SOUTH);
+		this.add(p3, BorderLayout.NORTH);
 
 		this.setVisible(true);
 	}
